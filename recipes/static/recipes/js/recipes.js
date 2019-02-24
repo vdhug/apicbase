62665e1eef7cc8854b18a4e093825038e1140b4f
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-02-24 19:05:29 -0300

    Creating new ingredients fields for recipes

--- recipes/static/recipes/js/recipes.js
+++ recipes/static/recipes/js/recipes.js
@@ -1,1 +1,19 @@
+// Template for adding a new ingredient for one recipe
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('#newIngredient').onclick = (e)  => {
+        e.preventDefault();
+        const template = Handlebars.compile(document.querySelector('#handlebarIngredientTemplate').innerHTML);
+        // Create new recipe element to DOM.
+
+        const content = template();
+        document.querySelector('#ingredients').innerHTML += content;
+    };
+
+});
+
+function removeRecipe() {
+    event.preventDefault();
+    event.target.parentElement.remove();
+}
