e90b2e3d82603f717ab0f374fd96a9ac925dc6f4
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-02-24 21:21:59 -0300

    Finishing recipe view

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.ingredients, name="ingredients"),
-    path("add", views.add_ingredient, name="add_ingredient"),
-    path("save", views.save_ingredient, name="save_ingredient"),
+    path("/add", views.add_ingredient, name="add_ingredient"),
+    path("/save", views.save_ingredient, name="save_ingredient"),
 ]
