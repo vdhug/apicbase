ae2fd4a7e04aeb6375d51af2691d139315f64095
Vitor Hugo <vitorhug97@gmail.com>
2019-02-26 14:00:06 -0500

    Finishing saving ingredient, addind redirect with message

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Ingredient
+
+
 # Create your views here.
 def ingredients(request):
+	
 	return render(request, "ingredients/index.html")
 
 
 def add_ingredient(request):
 	if request.method == "GET":
-		return render(request, "ingredients/ingredient.html")
+		context = {}
+		if request.session.has_key('result_message'):
+			context['resultMessage'] = request.session['result_message']
+			del request.session['result_message']
+		return render(request, "ingredients/ingredient.html", context)
 
 
 def save_ingredient(request):
@@ -36,7 +45,6 @@ def save_ingredient(request):
 			return render(request, "ingredients/ingredient.html", context)
 		else:
 			result = Ingredient.objects.save_ingredient(article_number=articleNumber, name=name,  base_amount=baseAmount, unit=unit, base_price=basePrice)
-			context = {
-				"resultMessage": result['message'],
-			}
-			return render(request, "ingredients/ingredient.html", context)
+			request.session['result_message'] = result['message']
+
+			return HttpResponseRedirect(reverse("add_ingredient"))
