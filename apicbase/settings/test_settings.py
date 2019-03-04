de784b946f298e83a1757fc444326f62e46f6eb5
Vitor Hugo <vitorhug97@gmail.com>
2019-03-04 16:12:46 -0500

    Substuting image for separator in listing objects

--- apicbase/settings/test_settings.py
+++ apicbase/settings/test_settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ingredients',
     'recipes',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+LOGIN_URL= '/'
